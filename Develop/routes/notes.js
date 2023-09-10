e591191c615ccb2c97960f01273e4db0916b3323
YJRose <yj.lin963320@gmail.com>
2023-09-10 19:12:44 -0400

    fixed code and added mock-up pictures

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,31 +1,37 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, deleteFile } = require('../helpers/fsUtils');
 const { v4: uuidv4 } = require('uuid');
 
 // GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for a new note
 notes.post('/', (req, res) => {
-  console.log(req.body);
+    console.log(req.body);
 
-  const { username, notestitle, textarea } = req.body;
+    const { title, text } = req.body;
 
-  if (req.body) {
-    const newNote = {
-      username,
-      notestitle,
-      textarea,
-      note_id: uuidv4(),
-    };
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            note_id: uuidv4(),
+        };
 
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully`);
-  } else {
-    res.error('Error in adding note');
-  }
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+// Delete Route for a note
+notes.delete('/user', (req, res) => {
+    console.log(note_id);
+
+    deleteFile('./db/db.json').then((data) => res.json(JSON.delete(data)));
 });
 
 module.exports = notes;
